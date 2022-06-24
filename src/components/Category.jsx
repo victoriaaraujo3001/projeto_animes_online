c9d54def863357fafb5175328dcc6811aa26d1e1
victoriaaraujo3001 <ketlenvictoria3009@gmail.com>
2022-06-24 10:52:38 -0300

    testando comit

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,22 @@
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
+import like from "../assets/like.png";
+import teste from "../assets/teste.png";
 
 import "../styles/styleCategory.css";
 
 export function Category() {
   const [listCategories, setListCategories] = useState([]);
 
+  const [boolean, setBoolean] = useState(false);
+
+  function change() {
+    if (!boolean) {
+      setBoolean(true);
+    } else {
+      setBoolean(false);
+    }
+  }
   async function CategoriesApi() {
     const categorias = await api.get("/categorias");
     setListCategories(categorias.data);
@@ -13,6 +24,7 @@ export function Category() {
   useEffect(() => {
     CategoriesApi();
   }, []);
+
   return (
     <>
       <main className="categorias">
@@ -29,9 +41,18 @@ export function Category() {
                       <img
                         className="img-section"
                         src={anime.imgAnime}
-                        alt=""
+                        alt="imgAnime"
                       />
-                    <h3 className="nome-anime">{anime.nome}</h3>
+                      <div className="button-like" onClick={() => change()}>
+                        {anime.favorito || boolean ?
+                            <img src={teste} alt="like" />
+
+                          :
+                          <img src={like} alt="like" />
+
+                        }
+                      </div>
+                      <h3 className="nome-anime">{anime.nome}</h3>
                     </div>
                   </>
                 ))}
